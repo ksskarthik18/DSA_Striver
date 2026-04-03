632ae7f589ec3b7b5029ff0ff276ecc0e792a87f
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-03 19:23:44 +0530

    pattern2

---
+++ Patterns/p2.cpp
@@ -0,0 +1,26 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+
+
+#include <iostream>
+using namespace std;
+
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    pattern2(n);
+}
