10f50e99862a0dd941f8abeea898cdbe92a4433e
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-12 11:33:13 +0530

    maximum subarray with sum k

---
+++ Arrays/Easy/max_subarray_with_sumk.py
@@ -0,0 +1,27 @@
+def longest_subarray_sumk(nums,k):
+    prefix_sum=0
+    max_len=0
+    hashmap={}
+
+    for i in range(len(nums)):
+        prefix_sum += nums[i]
+
+        if prefix_sum == k:
+            max_len=i+1
+        
+        if prefix_sum - k in hashmap :
+            length = i - hashmap[prefix_sum - k]
+            max_len = max(max_len,length)
+        
+        if prefix_sum not in hashmap:
+            hashmap[prefix_sum] = i
+        
+    return max_len
+    
+
+def main():
+    nums=[1,2,3,0,1,1,1,1,3,3]
+    k=3
+    print(longest_subarray_sumk(nums,k))
+
+main()
