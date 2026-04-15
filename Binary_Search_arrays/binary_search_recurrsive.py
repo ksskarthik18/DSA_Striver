54cf65c55e80c26a81ea780aedc677cd089c3562
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-15 21:53:48 +0530

    binary search using recurrsive approach

---
+++ Binary_Search_arrays/binary_search_recurrsive.py
@@ -0,0 +1,18 @@
+def binary_search_recurrsive(nums,low,high,target):
+    if low>high:
+        return -1
+    mid=(low+high)//2
+    if nums[mid]==target:
+        return mid
+    elif nums[mid]>target:
+        return binary_search_recurrsive(nums,low,mid-1,target)
+    else:
+        return binary_search_recurrsive(nums,mid+1,high,target)
+
+def main():
+    nums=[3,4,6,7,9,12,16,17]
+    target=9
+    n=len(nums)
+    print(binary_search_recurrsive(nums,0,n-1,target))
+
+main()
