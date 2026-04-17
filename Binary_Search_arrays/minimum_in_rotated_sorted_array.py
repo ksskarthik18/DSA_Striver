ffb0898540211cce0d2707277e0bf3b02be1fc15
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-17 20:35:27 +0530

    finding the minimum rotated sorted array

---
+++ Binary_Search_arrays/minimum_in_rotated_sorted_array.py
@@ -0,0 +1,22 @@
+#Time complexity : O(log n base 2)
+
+def get_minimum(nums):
+    n = len(nums)
+    low = 0
+    high = n -1
+    res= float('inf')
+    while low<=high:
+        mid = (low + high)//2
+        if nums[low]<=nums[mid]:
+            res = min(res,nums[low])
+            low = mid + 1
+        else:
+            res= min(res,nums[mid])
+            high = mid - 1
+        
+    return res
+
+def main():
+    nums = [3,4,5,1,2]
+    print(get_minimum(nums))
+main()
