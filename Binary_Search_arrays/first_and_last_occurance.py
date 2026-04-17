0a7f5e284de35ca85499d0aaf9780d3438ecc13f
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-17 08:14:31 +0530

    first and last occurance of num using binary search

---
+++ Binary_Search_arrays/first_and_last_occurance.py
@@ -0,0 +1,41 @@
+def first(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    first=-1
+    while low<= high:
+        mid = (low+high)//2
+        if nums[mid] == target:
+            first=mid
+            high = mid -1
+        elif nums[mid] < target:
+            low = mid + 1
+        else :
+            high = mid -1
+    
+    return first
+
+def last(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    last=-1
+    while low<=high:
+        mid = (low+high)//2
+        if nums[mid]==target:
+            last=mid
+            low = mid + 1
+        elif nums[mid]> target :
+            high= mid - 1
+        else :
+            low = mid + 1
+    return last
+
+def main():
+    nums = [5,7,7,8,8,10]
+    target = 8
+    fst=first(nums,target)
+    lst=last(nums,target)
+    print([fst,lst])
+main()
+
