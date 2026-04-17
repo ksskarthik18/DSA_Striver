2f4a77cc0a8e62cbf7f61e9dfae92e9b8de3374d
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-17 06:58:10 +0530

    lower bound

---
+++ Binary_Search_arrays/lower_bound.py
@@ -0,0 +1,24 @@
+#Time Complexity : O(log n base 2)
+
+def lower_bound(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    result=n
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]>=target:
+            result=mid
+            high=mid-1
+        else:
+            low=mid+1
+    
+    return result
+def main():
+    nums=[1,2,3,3,5,8,8,10,10,11]
+    target=8
+    print(lower_bound(nums,target))
+main()
+
+
+
