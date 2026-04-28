5f2eb2993110cb36c4d1ce4c7a4ac308c0ef2315
ksskarthik18 <karthik.kirla06@gmail.com>
2026-04-28 20:23:26 +0530

    maximum nesting depth and roman to integer

---
+++ Strings/Medium/maximum_nesting_depth_paren.py
@@ -0,0 +1,15 @@
+def find_max_depth(s) :
+    depth = 0
+    max_depth=0
+    for ch in s:
+        if ch == '(':
+            depth+=1
+            max_depth = max(depth,max_depth)
+        elif ch == ')':
+            depth -= 1
+    return max_depth
+
+def main():
+    s="(1)+((2))+(((3)))"
+    print(find_max_depth(s))
+main()
