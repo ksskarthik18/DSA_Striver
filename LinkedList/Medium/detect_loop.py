8bbad6844d2623c2c1e64ca031d87ebadc0d0f51
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-03 16:45:47 +0530

    detecting the loop

--- LinkedList/Medium/detect_loop.py
+++ LinkedList/Medium/detect_loop.py
@@ -1,4 +1,4 @@
-#Time Complexity : O(N/2)
+#Time Complexity : O(n)
 
 class Node:
     def __init__(self,value):
@@ -10,7 +10,6 @@ class LinkedList:
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-
     def append(self,value):
         new_node = Node(value)
         if self.head == None:
@@ -21,20 +20,22 @@ class LinkedList:
             self.tail = new_node
         return True
     
-    def find_middle_element(self):
+    def detect_loop(self):
         slow = self.head
         fast = self.head
-
         while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
-        return slow
-    
+            if slow == fast:
+                return True
+        return False
 
-my_linked_list = LinkedList(1)
+my_linked_list = LinkedList(3)
 my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
-
-print( my_linked_list.find_middle_element().value )
+my_linked_list.append(0)
+my_linked_list.append(-4)
+temp = my_linked_list.head
+for _ in range(1):
+    temp = temp.next
+my_linked_list.tail.next = temp
+print(my_linked_list.detect_loop())
