2d3e64d85c0bdfe3ff6237b7b9ab4afaae6d5c85
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-06 06:57:57 +0530

    done: merging the sorted arrays by avoiding the zeros

---
+++ LinkedList/Medium/sort_LL.py
@@ -0,0 +1,31 @@
+#Time Complexity : O (N/2)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+    
+    
