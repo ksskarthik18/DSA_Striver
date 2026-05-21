14c32e5ee48877798e3b7019dafe69ab6cd3fd04
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-21 08:46:11 +0530

    done: next greater element with 2 arrays

---
+++ Stack_&_Queues/Medium/next_greater_element1.py
@@ -0,0 +1,49 @@
+#Time Complexity : O(n+m)
+class Solution(object):
+    def __init__(self):
+        self.stack = []
+    def push(self,value):
+        self.stack.append(value)
+    
+    def pop(self):
+        if self.empty():
+            return "Stack Underflow"
+        return self.stack.pop()
+    
+    def top(self):
+        if self.empty():
+            return -1
+        return self.stack[-1]
+    def size(self):
+        return len(self.stack)
+    def empty(self):
+        return len(self.stack) == 0
+
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nge ={}
+        n2 = len(nums2)
+        for i in range(n2-1,-1,-1):
+            while not (self.empty()) and self.top()<= nums2[i]:
+                self.pop()
+            if self.empty():
+                nge[nums2[i]] = -1
+            else:
+                nge[nums2[i]] = self.top()
+            self.push(nums2[i])
+        result =[]
+        for num in nums1:
+            result.append(nge[num])
+        return result
+    
+def main():
+    s = Solution()
+    nums1 = [4,1,2]
+    nums2 = [1,3,4,2]
+    print(s.nextGreaterElement(nums1,nums2))
+main()
+    
