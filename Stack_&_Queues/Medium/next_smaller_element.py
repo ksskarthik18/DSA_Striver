73a69735c09e3dbd8f530d57c00efeb78b85ea10
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-21 12:22:11 +0530

    done: next smaller element

---
+++ Stack_&_Queues/Medium/next_smaller_element.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def __init__(self):
+        self.stack = []
+    def push(self,value):
+        self.stack.append(value)
+    
+    def pop(self):
+        if self.empty():
+            return "Stack Underflow"
+        return self.stack.pop()
+    
+    def top(self):
+        if self.empty():
+            return -1
+        return self.stack[-1]
+    def size(self):
+        return len(self.stack)
+    def empty(self):
+        return len(self.stack) == 0
+    
+    def nextSmallerElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        nge=[-1]*n
+        for i in range(n-1,-1,-1):
+
+            while not (self.empty()) and self.top() >= nums[i]:
+                self.pop()
+            if  self.empty():
+                nge[i]=-1
+            else:
+                nge[i] = self.top()
+            self.push(nums[i])
+
+        return nge
+
+
+def main():
+    nums=[1,2,1]
+    s = Solution()
+    print(s.nextSmallerElements(nums))
+main()      
