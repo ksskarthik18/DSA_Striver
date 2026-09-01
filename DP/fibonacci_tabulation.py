a4ad33c18f9473278e8917d19575a91591350c8a
ksskarthik18 <karthik.kirla06@gmail.com>
2026-09-01 12:00:00 +0530

    done: fibonacci using tabulation

---
+++ DP/fibonacci_tabulation.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    dp = [0]*(n+1)
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+n = 10
+for i in range(n):
+    print(fibonacci(i),end=" ")
